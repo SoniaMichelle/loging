61739284edee35f279f427454814b28b99900a1f
Michelle23 <68925851+SoniaMichelle@users.noreply.github.com>
2021-06-21 18:19:06 -0500

    Add files via upload

---
+++ registro/README.md
@@ -0,0 +1,8 @@
+# registro
+ 
+![image](https://user-images.githubusercontent.com/68925851/118304249-0ec38000-b4ac-11eb-9981-512cf5cbee74.png)
+![image](https://user-images.githubusercontent.com/68925851/118304307-1e42c900-b4ac-11eb-931c-5a334d720f86.png)
+![image](https://user-images.githubusercontent.com/68925851/118304316-226ee680-b4ac-11eb-8572-f7e07432c28c.png)
+![image](https://user-images.githubusercontent.com/68925851/118304345-2ef33f00-b4ac-11eb-95df-a9d66455177c.png)
+![image](https://user-images.githubusercontent.com/68925851/118304377-39153d80-b4ac-11eb-9966-2cc935e887fc.png)
+![image](https://user-images.githubusercontent.com/68925851/118304398-416d7880-b4ac-11eb-820a-54c53ee52e94.png)
---
+++ registro/view/registro.php
@@ -0,0 +1,27 @@
+
+            <form class="formulario">
+                <h3>Registro</h3>
+                <div class="contenedor">
+                    <div class="input-contenedor">
+                        <i class="fas fa-envelope icon"></i>
+                        <input 
+                          type="text" 
+                          placeholder="Usuario"
+                          id="us_mail"
+                          name="us_mail"
+                        >
+                    </div>
+                    <div class="input-contenedor">
+                        <i class="fas fa-key icon"></i>
+                        <input 
+                          type="password" 
+                          placeholder="Contraseña"
+                          id="us_password"
+                          name="us_password"
+                        >
+                    </div>
+                    <input type="button" value="Registrar" class="button" id="btn_registro">
+                    <p>¿Ya tienes una cuenta? <a class="link" href="login">Ingresar</a></p>
+                </div>
+            </form>
+<script src="manager/manager_registro.js"></script>
